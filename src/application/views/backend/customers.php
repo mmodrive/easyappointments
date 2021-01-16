f570fa6172795ab4ded4ab675e005af01972bef7
Alexey <mmodrive@gmail.com>
2021-01-16 17:02:04 +1000

    observations field swap in UI

--- src/application/views/backend/customers.php
+++ src/application/views/backend/customers.php
@@ -178,8 +178,8 @@
                                 <th><?= lang('depth') ?></th>
                                 <th><?= lang('speed') ?></th>
                                 <th><?= lang('time') ?></th>
-                                <th><?= lang('current').' '.lang('observations') ?></th>
                                 <th><?= lang('previous').' '.lang('observations') ?></th>
+                                <th><?= lang('current').' '.lang('observations') ?></th>
                             </tr>
                         </thead>
                         <tbody>
